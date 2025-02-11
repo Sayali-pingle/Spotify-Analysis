cedbdf635c1dde06d78e2c661cd33f482d064b19
svp27 <sayali9141@gmail.com>
2025-02-10 22:05:23 -0500

    deployment initial code

--- deploy.R
+++ deploy.R
@@ -1,1 +1,11 @@
- 
+library(rsconnect)
+
+# Read from environment variables
+rsconnect::setAccountInfo(
+    name = Sys.getenv("SHINYAPPS_NAME"),
+    token = Sys.getenv("SHINYAPPS_TOKEN"),
+    secret = Sys.getenv("SHINYAPPS_SECRET")
+)
+
+# Deploy the app
+rsconnect::deployApp() 
