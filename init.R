a7c8c711ca36808ae6cc055c0502bc574840a9ba
Sayali Pingle <72289957+Sayali-pingle@users.noreply.github.com>
2025-02-12 19:25:32 -0500

    Update init.R

--- init.R
+++ init.R
@@ -1,11 +1,11 @@
 # init.R
-my_packages = c("tidyverse", "shiny", "spotifyr", "shinydashboard", "plotly", 
-                "DT", "shinyWidgets", "waiter", "markdown", "dplyr", "ggplot2")
+# Remove or comment out the pak-related code
+# options(repos = c(CRAN = "https://cloud.r-project.org"))
+# if (!require("pak")) install.packages("pak")
+# pak::pkg_install(c("shiny", "shinydashboard", "tidyverse", "DT", "plotly", "waiter", "spotifyr"))
 
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
-  }
-}
-
-invisible(sapply(my_packages, install_if_missing)) 
+# Instead, use this:
+options(repos = c(RSPM = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
+required_packages <- c("shiny", "shinydashboard", "DT", "plotly", "waiter", "spotifyr")
+new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages) 
